0d90d76938d1b9ab712ff5218d37d5807568662b
Vahe Sevachyan <vscode13@gmail.com>
2021-11-16 19:35:30 -0800

    JS updates

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
+// const decimal = document.querySelector(".decimal");
+
+let firstValue = 0;
+let operatorValue = 0;
+let awaitingNextValue = false;
 
 const sendNumberValue = (number) => {
   const displayValue = calculatorDisplay.textContent;
@@ -8,12 +13,39 @@ const sendNumberValue = (number) => {
     displayValue === "0" ? number : displayValue + number;
 };
 
+const addDecimal = () => {
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+};
+
+const userOperator = (operator) => {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  }
+  operatorValue = operator;
+  console.log("fistValue", firstValue);
+  console.log("operator", operatorValue);
+};
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => userOperator(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue());
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+//reset display value
+const resetAll = () => {
+  firstValue = 0;
+  operatorValue = 0;
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = "0";
+};
+clearBtn.addEventListener("click", resetAll);
+
+// decimal.addEventListener("click", decimalInput);
